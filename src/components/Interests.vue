64831a9316af4c118e6376fb512b734af7842b31
recurn <61036489+recurn@users.noreply.github.com>
2021-05-29 15:58:39 -0700

    Adding in modal pop ups to interests

---
+++ src/components/Interests.vue
@@ -0,0 +1,142 @@
+<template>
+  <div id="interests">
+    <h1>Interests</h1>
+    <div id="interests-content">
+      <div @click="() => showModal(0)" class="interest">
+        <h3>Music</h3>
+        <img class="interest-icon" src="../assets/ukulele.png" alt="" />
+      </div>
+      <div @click="() => showModal(1)" class="interest">
+        <h3>D&D</h3>
+        <img class="interest-icon" src="../assets/dndDice.png" alt="" />
+      </div>
+      <div @click="() => showModal(2)" class="interest">
+        <h3>Meditation</h3>
+        <img class="interest-icon" src="../assets/meditation.png" alt="" />
+      </div>
+      <div @click="() => showModal(3)" class="interest">
+        <h3>Quantified Self</h3>
+        <img class="interest-icon" src="../assets/growth.png" alt="" />
+      </div>
+    </div>
+
+    <Modal v-show="isModalVisible[0]" @close="() => closeModal(0)">
+      <template v-slot:header>
+        <h3>Music</h3>
+      </template>
+      <template v-slot:body>
+        <p>
+          Music has always been a very important piece of my life. For me it's
+          an amazing creative outlet and a great way of conveying emotions.
+        </p>
+      </template>
+    </Modal>
+    <Modal v-show="isModalVisible[1]" @close="() => closeModal(1)">
+      <template v-slot:header>
+        <h3>Dungeons and Dragons</h3>
+      </template>
+      <template v-slot:body>
+        <p>D&D!!! WHOOO</p>
+      </template>
+    </Modal>
+    <Modal v-show="isModalVisible[2]" @close="() => closeModal(2)">
+      <template v-slot:header>
+        <h3>Meditation</h3>
+      </template>
+      <template v-slot:body>
+        <p>Meditation</p>
+      </template>
+    </Modal>
+    <Modal v-show="isModalVisible[3]" @close="() => closeModal(3)">
+      <template v-slot:header>
+        <h3>Quantified Self</h3>
+      </template>
+      <template v-slot:body>
+        <p>Measurement of self!</p>
+      </template>
+    </Modal>
+  </div>
+</template>
+
+<script>
+import Modal from "@/components/Modal.vue";
+
+export default {
+  name: "App",
+  components: {
+    Modal,
+  },
+  data() {
+    return {
+      isModalVisible: [false, false, false, false],
+    };
+  },
+  methods: {
+    showModal(i) {
+      this.isModalVisible[i] = true;
+    },
+    closeModal(i) {
+      this.isModalVisible[i] = false;
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.interest {
+  cursor: pointer;
+  border-radius: 20px;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  & h3 {
+    margin: 0;
+  }
+
+  &:hover {
+    .interest-icon {
+      filter: invert(0%) sepia(0) saturate(0) hue-rotate(0) brightness(0%)
+        contrast(1000%);
+    }
+    color: black;
+    //background: rgba(255, 255, 255, 0.1);
+    filter: invert(37%) sepia(88%) saturate(3024%) hue-rotate(324deg)
+      brightness(92%) contrast(84%);
+  }
+}
+.interest-icon {
+  filter: invert(9%) sepia(14%) saturate(343%) hue-rotate(358deg)
+    brightness(113%) contrast(103%);
+  max-width: 150px;
+}
+#interests {
+  color: var(--ivory);
+  margin: auto;
+  justify-content: center;
+  align-content: center;
+  align-items: center;
+  padding: 20px;
+  margin: 0 auto;
+  background: var(--primary);
+}
+
+#interests h2 {
+  margin: 5px 10px;
+}
+
+#interests-content {
+  display: grid;
+  align-items: center;
+  text-align: center;
+
+  grid-template-columns: repeat(auto-fit, minmax(175px, 1fr));
+  grid-gap: 30px;
+  justify-content: center;
+  margin: auto;
+  max-width: 1200px;
+  padding: 0px 1em;
+}
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,131 @@
+<script>
+export default {
+  name: "Modal",
+  methods: {
+    close() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
+
+<template>
+  <transition name="modal-fade">
+    <div class="modal-container">
+      <div class="modal-backdrop" @click="close"></div>
+      <div
+        class="modal"
+        role="dialog"
+        aria-labelledby="modalTitle"
+        aria-describedby="modalDescription"
+        @click.prevent=""
+      >
+        <header class="modal-header" id="modalTitle">
+          <slot name="header">
+            This is the default tile!
+          </slot>
+        </header>
+
+        <section class="modal-body" id="modalDescription">
+          <slot name="body">
+            This is the default body!
+          </slot>
+        </section>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<style>
+.modal-container {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 9997;
+}
+
+.modal-backdrop {
+  position: fixed;
+  background-color: rgba(0, 0, 0, 0.6);
+  z-index: 9998;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+}
+
+.modal {
+  background: #ffffff;
+  border-radius: 10px;
+  box-shadow: 2px 2px 20px 1px rgb(100, 100, 100);
+  overflow-x: auto;
+  display: flex;
+  flex-direction: column;
+  z-index: 9999;
+  max-width: 60%;
+  min-width: 300px;
+}
+
+.modal-header,
+.modal-footer {
+  padding: 15px;
+  display: flex;
+}
+
+.modal-header {
+  position: relative;
+  border-bottom: 1px solid #eeeeee;
+  color: var(--primary);
+  justify-content: center;
+}
+
+.modal-footer {
+  border-top: 1px solid #eeeeee;
+  flex-direction: column;
+}
+
+.modal-body {
+  position: relative;
+  padding: 20px 10px;
+  color: var(--primary);
+}
+
+.btn-close {
+  position: absolute;
+  top: 0;
+  right: 0;
+  height: 10px;
+  width: 10px;
+  border: none;
+  margin: 0;
+  padding: 0;
+  font-size: 20px;
+  /* padding: 10px; */
+  cursor: pointer;
+  font-weight: bold;
+  color: #4aae9b;
+  background: transparent;
+}
+
+.btn-green {
+  color: white;
+  background: #4aae9b;
+  border: 1px solid #4aae9b;
+  border-radius: 2px;
+}
+
+.modal-fade-enter-from,
+.modal-fade-leave-to {
+  opacity: 0;
+}
+
+.modal-fade-enter-active,
+.modal-fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+</style>
